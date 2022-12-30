d3696dea526812b86ad7dd2903397fd98e84283e
leogonzalez83 <121648642+leogonzalez83@users.noreply.github.com>
2022-12-29 19:35:34 -0600

    Add files via upload

---
+++ web-project/test-site/scripts/main.js
@@ -0,0 +1,40 @@
+let miImage = document.querySelector('img');
+miImage.onclick = function () {
+    let miSrc = miImage.getAttribute('src');
+    if (miSrc === 'images/foto-navidad-rosada.jpg') {
+      miImage.setAttribute('src','images/familia.png');
+    } else {
+      miImage.setAttribute('src', 'images/foto-navidad-rosada.jpg');
+    }
+}
+let miBoton = document.querySelector('button');
+let miTitulo = document.querySelector( 'h1');
+
+function estableceNombreUsuario() {
+    let miNombre = prompt('Por favor, ingresa tu nombre.');
+    localStorage.setItem('nombre', miNombre);
+    miTitulo.textContent = 'Mozilla es genial,' + miNombre;
+}
+
+if (!localStorage.getItem('nombre')) {
+    estableceNombreUsuario();
+}
+else {
+    let nombreAlmacenado = localStorage.getItem('nombre');
+    miTitulo.textContent = 'Mozilla es genial,' + nombreAlmacenado;
+}
+
+miBoton.onclick = function() {
+    estableceNombreUsuario();
+}
+
+function estableceNombreUsuario() {
+    let miNombre = prompt('Introduzca su nombre.');
+    if(!miNombre) {
+      estableceNombreUsuario();
+    } else {
+      localStorage.setItem('nombre', miNombre);
+      miTitulo.innerHTML = 'Mozilla is genial, ' + miNombre;
+    }
+  }
+  
